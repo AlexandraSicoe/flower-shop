58e284a3374ced359b5f3339906a4f69537e83ee
Alexandra Sicoe <sicoealexandra97@gmail.com>
2023-07-20 18:36:03 +0300

    finished cart and checkout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,18 @@
 import { Grid } from "@mui/joy";
 import Typography from "@mui/joy/Typography";
 import Button from "@mui/joy/Button";
+import { useEffect, useState } from "react";
 
 const Navbar = ({ cart }) => {
+  const [totalPrice, setTotalPrice] = useState(0); //cand schimbam un state nu avem =
+  useEffect(() => {
+    let sum = 0;
+    cart.forEach((flower) => {
+      sum = sum + flower.price;
+    });
+    setTotalPrice(sum);
+  }, [cart]);
+
   return (
     <Grid
       xs={12}
@@ -24,38 +34,60 @@ const Navbar = ({ cart }) => {
       <div
         style={{
           display: "flex",
-          gap: 2,
-          flexWrap: "wrap",
-          marginLeft: "25px",
-          height: "40px",
-          position: "relative",
+          alignItems: "center",
         }}
       >
-        <Button size="md" sx={{ fontSize: "20px" }}>
-          <ion-icon name="cart"></ion-icon>
-        </Button>
-        {cart.length > 0 && (
-          <div
-            style={{
-              position: "absolute",
-              backgroundColor: "red",
-              width: "25px",
-              height: "25px",
-              zIndex: 999,
-              top: -10,
-              right: -10,
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              borderRadius: "50%",
-              paddingBottom: "2px",
-            }}
-          >
-            <Typography sx={{ color: "white" }} level="body2">
-              {cart.length}
-            </Typography>
-          </div>
-        )}
+        <div
+          style={{
+            backgroundColor: "white",
+            height: "40px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: "10%",
+            padding: "5px",
+          }}
+        >
+          <Typography sx={{ color: "black" }} level="body1">
+            {totalPrice} RON
+          </Typography>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            gap: 2,
+            flexWrap: "wrap",
+            marginLeft: "10px",
+            height: "40px",
+            position: "relative",
+          }}
+        >
+          <Button size="md" sx={{ fontSize: "20px" }}>
+            <ion-icon name="cart"></ion-icon>
+          </Button>
+          {cart.length > 0 && (
+            <div
+              style={{
+                position: "absolute",
+                backgroundColor: "red",
+                width: "25px",
+                height: "25px",
+                zIndex: 999,
+                top: -10,
+                right: -10,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                borderRadius: "50%",
+                paddingBottom: "2px",
+              }}
+            >
+              <Typography sx={{ color: "white" }} level="body2">
+                {cart.length}
+              </Typography>
+            </div>
+          )}
+        </div>
       </div>
     </Grid>
   );
