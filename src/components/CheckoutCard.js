6884f3b36f28074e4cc74b2f3a13f20ae1942325
Bogdan Falk <bogdan@digitalgarden.ro>
2023-07-24 15:49:47 +0300

    updated CheckoutCard props

--- src/components/CheckoutCard.js
+++ src/components/CheckoutCard.js
@@ -5,7 +5,7 @@ const CheckoutCard=({flower})=>{
         
             <Box sx={{display: "flex", justifyContent: "flex-start", alignItems: "flex-end", flexDirection: "row", marginTop:"15px", marginLeft:"15px", borderBottom: "2px solid black", padding:"10px"}}>
                 <Box> 
-                <img style={{width:"100%", height:"150px"}} src="https://images.unsplash.com/photo-1518895949257-7621c3c786d7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80"></img>
+                <img style={{width:"100%", height:"150px"}} src={flower.image}></img>
                 </Box>
                 <Box sx={{display: "flex", justifyContent: "space-between",  flexDirection: "row", width:"300px", height:"100%", marginLeft:"10px"}}>
                 <Typography level="body1">
